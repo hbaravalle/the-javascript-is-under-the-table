6bc226b252f92915d4b2b6bd3470adb1bde33ccb
Hernán Baravalle <hernan.baravalle@gmail.com>
2019-07-04 00:12:56 -0300

    Nueva lógica, índices y posiciones relativas al window.innerWidth

--- mmonks/js/main.js
+++ mmonks/js/main.js
@@ -1,28 +1,49 @@
 window.onload = function() {
-  const backgroundImage = new Image();
-  backgroundImage.src = './img/background.jpg';
 
-  let totalWidth = backgroundImage.width;
-  let anEighth = parseInt(totalWidth / 8);
+  const laImagen = new Image();
+  laImagen.src = './img/background.jpg';
+  let anchoTotalImagen = parseInt(laImagen.width);
 
   let main = document.querySelector('main');
-  let buttonLeft = document.querySelector('#buttonLeft');
-  let buttonRight = document.querySelector('#buttonRight');
+  let flechaIzquierda = document.querySelector('#buttonLeft');
+  let flechaDerecha = document.querySelector('#buttonRight');
+  let listaDeSlides = document.querySelectorAll('.inner-slide');
+  let cantidadDeSlides = listaDeSlides.length;
+
+  let indice = 1;
+
+  if (indice == 1) {
+    buttonLeft.classList.add('disabled');
+  }
 
   buttonLeft.addEventListener('click', function() {
     let mainStyles = getComputedStyle(main);
     let actualPosition = parseInt(mainStyles.backgroundPositionX);
-    if(actualPosition !== 0) {
-      main.style.backgroundPositionX = actualPosition + anEighth + 'px';
-      console.log('derecha derecha');
+
+    if(indice > 1) {
+      indice--;
+      main.style.backgroundPositionX = actualPosition + unOctavoDeLaImagen + 'px';
+      console.log('<<<<<<<< Izquierda');
+    } else {
+      console.log("Imagen en posición inicial");
     }
   });
 
   buttonRight.addEventListener('click', function() {
     let mainStyles = getComputedStyle(main);
     let actualPosition = parseInt(mainStyles.backgroundPositionX);
-    main.style.backgroundPositionX = actualPosition - anEighth + 'px';
-    console.log('izquierda izquierda')
+
+    if (indice >= 1 && indice < 8) {
+      indice++;
+      console.log(indice)
+      main.style.backgroundPositionX = actualPosition - unOctavoDeLaImagen + 'px';
+      console.log('Derecha >>>>>>>>');
+    } else if (indice === 8) {
+      indice++;
+      main.style.backgroundPositionX = "right";
+    } else {
+
+    }
   });
 
 }
