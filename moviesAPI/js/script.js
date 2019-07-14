504cc785de35f8df318b4081f4f1dd4192f067bc
Hernán Baravalle <hernan.baravalle@gmail.com>
2019-07-14 00:37:26 -0300

    moviesAPI - Se incluye API Movies + bucle para crear cards de pelis

--- moviesAPI/js/script.js
+++ moviesAPI/js/script.js
@@ -1,22 +1,34 @@
-const superJson = null;
-fetch('https://api.themoviedb.org/3/movie/top_rated?page=1&language=en-US&api_key=81b468865772aad355fffbae68ca1383') // Call the fetch function passing the url of the API as a parameter
-.then(function(response) {
-    return response.json()
-})
-.then(function(elJson) {
+window.onload = function() {
+  fetch('https://api.themoviedb.org/3/movie/popular?api_key=81b468865772aad355fffbae68ca1383&language=es&page=1')
 
+    .then(function(res) {
+      return res.json();
+    })
+    .then(function(data) {
+      let main = document.querySelector('main');
+      let pelis = data.results;
+      console.log(pelis);
 
-  console.log(elJson.results);
-  let jsonFinal = elJson.results;
-  let lista = document.querySelector('#listaPeliculas');
+      for(let i = 0; i < pelis.length; i++) {
+        let unaPeli = {
+          id: pelis[i].id,
+          generos: pelis[i].genre_ids,
+          titulo: pelis[i].title,
+          sinopsis: pelis[i].overview,
+          backdrop: pelis[i].backdrop_path,
+          poster: pelis[i].poster_path,
+          ranking: pelis[i].vote_average,
+          estreno: pelis[i].release_date
+        }
 
-  for(var i = 0; i < jsonFinal.length; i++) {
-    console.log(jsonFinal[i]);
-    let li = `<div>${jsonFinal[i].title}</div>`
-    lista.innerHTML += li
-  }
+        let markup 0 = `
+          <article class="card-pelicula">
+            <img src="" alt="">
+            <h2>${unaPeli.titulo}</h2>
+            <span>ranking</span>
+          </article>
+        `
+      }
+    })
 
-})
-.catch(function() {
-  console.log("todo mal");
-});
+}
